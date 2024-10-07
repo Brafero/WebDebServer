998cea93d3fc5b7e9f5e3daf8162efdf763fcbe1
Brafero <139059957+Brafero@users.noreply.github.com>
2024-10-07 20:03:46 -0300

    Update hello.py
    Exercicio aula online

--- hello.py
+++ hello.py
@@ -3,17 +3,46 @@ from flask import Flask, request, make_response, redirect, abort
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<p>Hello from Flask!</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
-
+def home():
+    return '''
+    <html>
+        <head>
+            <title>Avaliação contínua: Aula 030</title>
+        </head>
+        <body>
+            <h1>Avaliação contínua: Aula 030</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/identificacao/Matheus%20Braga/PT3025446/IFSP">Identificação</a></li>
+                <li><a href="/contextorequisicao">Contexto da requisição</a></li>
+            </ul>
+        </body>
+    </html>
+    '''
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
 
 @app.route('/contextorequisicao')
 def contextorequisicao():
-    user_agent = request.headers.get('User-agent')
-    return '<p>Seu navegador é {}! </p>'.format(user_agent);
+    user_agent = request.headers.get('User-Agent')
+    ip_remoto = request.remote_addr
+    host_aplicacao = "https://brafero.pythonanywhere.com/"
+
+    return '''
+    <html>
+        <head>
+            <title>Contexto da Requisição</title>
+        </head>
+        <body>
+            <h1>Avaliação contínua: Aula 030</h1>
+            <h2><b>Seu navegador é:</b> {}</h2>
+            <h2><b>O IP do computador remoto é:</b> {}</h2>
+            <h2><b>O host da aplicação é:</b> {}</h2>
+            <a href="/">Voltar</a>
+        </body>
+    </html>
+    '''.format(user_agent, ip_remoto, host_aplicacao)
 
 @app.route('/codigostatusdiferente')
 def codigostatusdiferente():
@@ -33,3 +62,20 @@ def redirecionamento():
 @app.route('/abortar')
 def abortar():
     return abort(404)
+
+@app.route('/identificacao/<nome>/<prontuario>/<instituicao>')
+def identificacao(nome, prontuario, instituicao):
+    return '''
+    <html>
+        <head>
+            <title>Identificação</title>
+        </head>
+        <body>
+            <h1>Avaliação contínua: Aula 030</h1>
+            <h2><b>Aluno:</b> {}</h2>
+            <h2><b>Prontuário:</b> {}</h2>
+            <h2><b>Instituição:</b> {}</h2>
+            <a href="/">Voltar</a>
+        </body>
+    </html>
+    '''.format(nome, prontuario, instituicao)
